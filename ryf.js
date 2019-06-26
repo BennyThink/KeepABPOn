441fdd03eaa537b061d05decd3bea9012418a6bc
Benny <benny.think@gmail.com>
2019-06-26 15:33:44 +0800

    fix ryf style

--- ryf.js
+++ ryf.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KAO#1:ruanyifeng
 // @namespace    https://www.bennythink.com/
-// @version      0.2
+// @version      0.3
 // @description  浏览阮一峰博客时，不必关闭广告屏蔽器。
 // @author       BennyThink
 // @supportURL   https://github.com/BennyThink/KeepABPOn
@@ -13,6 +13,15 @@
 
 var ryf = document.querySelector('#main-content').innerHTML;
 
+function loadjscssfile(filename) {
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", filename);
+    if (typeof fileref != "undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
 function ruanyifeng() {
 
     document.getElementsByClassName('asset-meta')[0].nextElementSibling.style = 'display:none';
@@ -20,6 +29,8 @@ function ruanyifeng() {
 }
 
 setTimeout(ruanyifeng, 1001);
+
+loadjscssfile('/static/themes/theme_scrapbook/theme_scrapbook.css');
 
 
 
